79af3403df73c1299052e1ed0ea5987b7beda2dc
andressa0102 <andressas.as12352gmail.com>
2023-03-21 13:53:48 -0300

    app

---
+++ src/screens/Slider1/index.tsx
@@ -0,0 +1,6 @@
+import { FlatList, ImageBackground, View } from 'react-native';
+import { IPage } from '../../../App';
+import {
+    ComponentButtonSlider, ComponentListMarker, ComponentTitleSlider
+} from '../../components';
+import { styles } from './styles';
