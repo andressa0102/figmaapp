951689bcc0f123f6a7d32b322a2f9cb482cccae9
andressa0102 <andressas.as1235@gmail.com>
2023-04-03 12:29:47 -0300

    app

---
+++ src/screens/Login/styles.ts
@@ -0,0 +1,36 @@
+import { StyleSheet } from 'react-native';
+import { colors  } from '../../styles/colors';
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: "bold",
+        textAlign: "center",
+        colors: colors.black
+    },
+    formRow: {
+        margin: 10,
+        flexDirection: "row",
+        alignItems: "center",
+        borderWidth: 1,
+        borderColor: colors.primary,
+        borderRadius: 5
+
+    },
+    icon: {
+        fontSize: 18,
+        color: colors.primary,
+        padding: 5
+
+    },
+    input: {
+        fontSize: 18,
+        padding: 10,
+        width: "70%"
+
+    }
+})
