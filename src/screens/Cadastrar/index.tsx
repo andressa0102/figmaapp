38cccb8272200718b8efcc52c4a169748474165a
andressa0102 <andressas.as1235@gmail.com>
2023-04-10 12:11:14 -0300

    mensagem

--- src/screens/Cadastrar/index.tsx
+++ src/screens/Cadastrar/index.tsx
@@ -2,14 +2,22 @@ import React from "react";
 import { View, Text, KeyboardAvoidingView, TextInput } from "react-native" 
 import { styles } from "./styles"
 import { MaterialIcons, Entypo } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 import { colors } from "../../styles/colors";
 import { ComponentButtonInterface } from "../../components"
 
-export function Login() {
+export function Cadastrar() {
     return (
         <View style={styles.container}>
             <KeyboardAvoidingView>
-                <Text style ={styles.title}>Login</Text>
+                <Text style ={styles.title}>Cadastre-se</Text>
+                <View style={styles.formRow}>
+                    <Ionicons name="person" size={24} color="black" />
+                    <TextInput
+                        placeholder="Nome"
+                        style={styles.input}
+                    />
+                </View>
                 <View style={styles.formRow}>
                     <MaterialIcons name="email" size={24} color="black" />
                     <TextInput
@@ -25,13 +33,13 @@ export function Login() {
                     <TextInput
                         placeholder="Senha"
                         placeholderTextColor={colors.black}
-                        secureTextEntry={true} //fazer com que a senha fique com bolinha
+                        secureTextEntry={true}
                         autoCapitalize="none"
                         style={styles.input}
                     />
                 </View>
-                <ComponentButtonInterface title="Entrar" type="primary" onPressI={()=>{console.log("Login")}} />
-                <ComponentButtonInterface title="Cadastrar" type="primary" onPressI={()=>{console.log("Cadastrar")}} />
+                <ComponentButtonInterface title="Salvar" type="primary" onPressI={()=>{console.log("Cadastrar")}} />
+                <ComponentButtonInterface title="Voltar" type="primary" onPressI={()=>{console.log("Login")}} />
             </KeyboardAvoidingView>
         </View>
     
--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -3,6 +3,7 @@ export const colors = {
     secondary: '#9C5F22',
     third: '#E0AF7E',
     thirdLight: 'rgba(188, 151, 114, 0.54);',
-    black: '#000'
+    black: '#000000',
+    white: "#ffffff"
 
 }
