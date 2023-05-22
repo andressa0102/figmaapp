134be4483b95d41965c04854782fb06ced0fb1dc
anddressa0102 <andressas.as1235@gmail.com>
2023-05-22 12:23:40 -0300

    mensagem

--- src/screens/Camera/index.tsx
+++ src/screens/Camera/index.tsx
@@ -1,32 +1,50 @@
-import { Camera, CameraType } from 'expo-camera';
-import React, { Component, useRef } from 'react';
-import { useState } from 'react';
-import { Button, Text, TouchableOpacity, View } from 'react-native';
-import { ComponentButtonInterface } from '../../components';
-import {styles} from "./styles"
+import { Camera, CameraCapturedPicture, CameraType } from 'expo-camera';
+import { useState, useRef } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View, Image, Alert } from 'react-native';
+import { ComponentButtonInterface, ComponentButtonTakePicture } from '../../components';
+import { styles } from './styles';
+import { AntDesign } from '@expo/vector-icons';
+import * as ImagePicker from "expo-image-picker";
+import * as MediaLibrary from "expo-media-library"
 interface IPhoto {
-    height: string
-    uri: string
-    width: string
+  height: string
+  uri: string
+  width: string
 }
 
 export function CameraScreen() {
   const [type, setType] = useState(CameraType.back);
-  const [permission, requestPermission] = Camera.useCameraPermissions();
-  const [photo, setphoto] = useState<IPhoto>()
-  const ref = useRef(null)
+  const [permissionCamera, requestPermissionCamera] = Camera.useCameraPermissions();
+  const [permissionMedia, requestPermissionMedia] = MediaLibrary.usePermissions()
+  const [photo, setPhoto] = useState<CameraCapturedPicture | ImagePicker.ImagePickerAsset>()
+  const ref = useRef<Camera>(null)
+  const [takePhoto, setTakePhoto] = useState(false)
 
-  if (!permission) {
+  if (!permissionCamera) {
     // Camera permissions are still loading
     return <View />;
   }
 
-  if (!permission.granted) {
+  if (!permissionCamera.granted) {
     // Camera permissions are not granted yet
     return (
       <View style={styles.container}>
-        <Text style={{ textAlign: 'center' }}>Permita o acesso a sua câmera!</Text>
-        <Button onPress={requestPermission} title="grant permission" />
+        <Text style={{ textAlign: 'center' }}>Precisamos de permissão para acessar a câmera!!!</Text>
+        <Button onPress={requestPermissionCamera} title="grant permission" />
+      </View>
+    );
+  }
+  if (!permissionMedia) {
+    // Camera permissions are still loading
+    return <View />;
+  }
+
+  if (!permissionMedia.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>Precisamos de permissão para acessar a câmera!!!</Text>
+        <Button onPress={requestPermissionMedia} title="grant permission" />
       </View>
     );
   }
@@ -36,21 +54,44 @@ export function CameraScreen() {
   }
 
   async function takePicture() {
-    if(ref.current) {
-        const picture = await ref.current.takePictureAsync()
-        console.log(picture)
-        setphoto(picture)
+    if (ref.current) {
+      const picture = await ref.current.takePictureAsync()
+      console.log(picture)
+      setPhoto(picture)
+    }
+  }
+
+  async function savePhoto() {
+    const asset = await MediaLibrary.createAssetAsync(photo!.uri)
+    MediaLibrary.createAlbumAsync("Images", asset, false)
+    Alert.alert("Imagem salva com sucesso!")
+  }
+
+  async function pickImage() {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1
+    })
+    if (!result.canceled) {
+      setPhoto(result.assets[0])
     }
   }
 
   return (
     <View style={styles.container}>
-        <ComponentButtonInterface title='Flip' type='secondary' onPressI={toggleCameraType} />
-        <Camera style={styles.camera} type={type} ref={ref} />
-        <ComponentButtonInterface title='Tirar Foto' type='secondary' onPressI={takePicture} />
-        {photo && photo.uri && (
-            <image source={{ uri: photo.uri }} style={styles.img} />
-        )}
+      <Camera style={styles.camera} type={type} ref={ref}>
+        <TouchableOpacity onPress={toggleCameraType} style={styles.container}>
+          <AntDesign name="retweet" size={40} color="black" type='secondary' />
+        </TouchableOpacity>
+      </Camera>
+      <ComponentButtonInterface title='Tirar Foto' type='secondary' onPressI={takePicture} />
+      <ComponentButtonInterface title='Salvar Imagem' type='secondary' onPressI={savePhoto} />
+      <ComponentButtonInterface title='Abrir Imagem' type='secondary' onPressI={pickImage} />
+
+      {photo && photo.uri && (
+        <Image source={{ uri: photo.uri }} style={styles.img} />
+      )}
     </View>
   );
 }
