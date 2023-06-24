b93e957ea5d2def554a3e99329fbed0a7b16deea
andressa0102 <andressas.as1235@gmail.com>
2023-06-24 15:20:10 -0300

    mensagem

--- src/screens/Location/index.tsx
+++ src/screens/Location/index.tsx
@@ -7,6 +7,7 @@ import { styles } from "./styles"
 import { colors } from '../../styles/colors';
 import {GooglePlacesAutocomplete, GooglePlaceData, GooglePlaceDetail} from 'react-native-google-places-autocomplete';
 import MapViewDirections from 'react-native-maps-directions'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export function LocationScreen() {
   const [location, setLocation] = useState<Location.LocationObject | null>(null);
@@ -59,29 +60,85 @@ export function LocationScreen() {
       }
     }
   }, []);
+
+  async function handleDestination(data: GooglePlaceData, details: GooglePlaceDetail | null) {
+    if (details) {
+      setDestination({
+        latitude: details?.geometry.location.lat,
+        longitude: details?.geometry.location.lng,
+        latitudeDelta: 0.004,
+        longitudeDelta: 0.004
+      })
+      if (marker) {
+        setMarker([...marker, {
+          latitude: details?.geometry.location.lat,
+          longitude: details?.geometry.location.lng,
+          latitudeDelta: 0.004,
+          longitudeDelta: 0.004
+        }])
+      }
+    }
+  }
   let text = 'Waiting..';
   if (errorMsg) {
     text = errorMsg;
   } else if (location) {
     text = JSON.stringify(location);
+  } else if (destination) {
+    text = JSON.stringify(destination);
   }
 
   return (
     <View style={styles.container}>
-        {region ? (
-            <MapView region={region} style={styles.map} >
-                {marker && marker.map((i) => (
-                    <Marker key={i.latitude} coordinate={i} />
-                ))}
-                {coords && <Polyline 
-                  coordinates={coords}
+      <GooglePlacesAutocomplete
+        styles={{ container: styles.searchContainer, textInput: styles.searchInput }}
+        placeholder="Para onde vamos?"
+        fetchDetails={true}
+        GooglePlacesDetailsQuery={{ fields: "geometry" }}
+        enablePoweredByContainer={false}
+        query={{
+          key: API_GOOGLE,
+          language: 'pt-BR'
+        }}
+        onFail={setErrorMsg}
+        onPress={handleDestination}
+      />
+      {region ? (
+          <MapView region={region} style={styles.map} ref={mapRef} showsUserLocation={true} >
+              {marker && marker.map((i) => (
+                  <Marker key={i.latitude} coordinate={i} >
+                    <MaterialCommunityIcons name="cellphone-marker" sizze={48} color={colors.black} />
+                  </Marker>
+              ))}
+              {coords && <Polyline 
+                coordinates={coords}
+                strokeColor={colors.black}
+                strokeWidth={7} />
+              }
+              {destination && (
+                <MapViewDirections
+                  origin={region}
+                  destination={destination}
+                  apikey={API_GOOGLE}
                   strokeColor={colors.black}
                   strokeWidth={7}
-                />}
-            </MapView>
-        ) : (
-            <Text style={styles.paragraph}>{text}</Text>
-        )}
+                  lineDashPattern={[0]}
+                  onReady={(result) => {
+                    mapRef.current?.fitToCoordinates(result.coordinates, {
+                      edgePadding: {
+                        top: 24,
+                        bottom: 24,
+                        left: 24,
+                        right: 24
+                      }
+                    })
+                  }}
+                />
+              )}
+          </MapView>
+      ) : (
+          <Text style={styles.paragraph}>{text}</Text>
+      )}
     </View>
   );
 }
